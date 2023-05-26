adeb2f3b15da0a692ccd08a9723230211487ab9a
Billit Shotta <billitshotta@gmail.com>
2023-05-26 19:49:03 +0700

    container , property text and styling

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -10,10 +10,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("MyApp"),),
-        body: Center(child: const Text("Hello world")),        
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("MyApp"),
+      ),
+      body: Center(
+          child: Container(
+              color: Colors.lightBlue,
+              height: 100,
+              width: 150,
+              child: const Text(
+                "Saya sedang belajar melatih keterampilan flutter.",
+                maxLines:5,
+                overflow: TextOverflow.visible,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontStyle:FontStyle.italic,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 11,
+                ),
+              ))),
+    ));
   }
 }
